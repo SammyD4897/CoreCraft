e7058297f551111f0ed6e66f63421306c9b8ec4d
SammyD4897 <shadowslamer@gmail.com>
2015-04-08 21:57:29 -0500

    First Mod Class

---
+++ src/main/java/com/PoiseEn/core/CoreCraft.java
@@ -0,0 +1,27 @@
+package com.PoiseEn.core;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="CORECraft", name="C.O.R.E. Craft", version="1.8-1.0")
+public class CoreCraft {
+    @Mod.Instance("CORECraft")
+    public static CoreCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
